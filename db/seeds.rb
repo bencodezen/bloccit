446772aea1ee1e76586281d08917d544a203fccb
bengozen <bengozen@gmail.com>
2014-09-01 19:43:17 -0400

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require 'faker'
 
-#Create Users
-
+# Create Users
 5.times do
   user = User.new(
     name:       Faker::Name.name,
@@ -13,17 +12,27 @@ require 'faker'
 end
 users = User.all
 
+# Create Topics
+15.times do
+  Topic.create!(
+    name:           Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
   Post.create!(
     user:    users.sample,
+    topic:   topics.sample,
     title:    Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
 end
 posts = Post.all
 
-# Create comments
+# Create Comments
 100.times do
   Comment.create!(
     post:     posts.sample,
@@ -31,10 +40,34 @@ posts = Post.all
   )
 end
 
-User.first.update_attributes!(
-  email: 'bengozen@gmail.com',
-  password: 'dragonHong88'
+# Create an admin user
+admin = User.new(
+  name:       'Admin User',
+  email:       'admin@example.com',
+  password:  'helloworld',
+  role:           'admin'
 )
+admin.skip_confirmation!
+admin.save!
+
+# Create a moderator
+moderator = User.new(
+  name:       'Moderator User',
+  email:       'moderator@example.com',
+  password:  'helloworld',
+  role:           'moderator'
+)
+moderator.skip_confirmation!
+moderator.save!
+
+# Create a member
+member = User.new(
+  name:         'Member User',
+  email:          'member@example.com',
+  password:    'helloworld'
+)
+member.skip_confirmation!
+member.save!
 
 puts "Seed finished"
 puts "#{User.count} users created"
