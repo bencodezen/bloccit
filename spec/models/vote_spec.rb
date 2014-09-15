851dbbcccc88cfc441c9d0845f26748d612862b4
bengozen <bengozen@gmail.com>
2014-09-15 14:17:21 -0400

    Completed Votes exercise.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 describe Vote do
   describe "validations" do
-    descrive "value validation" do
+    describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( @vote.down_vote ).to eq(1)
-        expect( @vote.up_vote ).to eq(-1)
+        v = Vote.new(value: 1)
+        expect( v.valid? ).to eq(true)
+        v2 = Vote.new(value: 2)
+        expect( v2.valid? ).to eq(false)
+        v3 = Vote.new(value: -1)
+        expect( v3.valid? ).to eq(true)
       end
     end
   end
-end
-
 end
