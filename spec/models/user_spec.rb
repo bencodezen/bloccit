da08104c7abe435363eb6209ffb7c9ed18ac7592
bengozen <bengozen@gmail.com>
2014-09-16 14:57:16 -0400

    Added favorites functionality.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe User do
+
+  include TestFactories
+
+  before do
+    @post = associated_post
+    @user = authenticated_user
+  end
+
+  describe "#favorited(post)" do
+    it "returns 'nil' if the user has not favorited the post" do
+      expect( @user.favorites.find_by_post_id(@post.id) ).to eq(nil)
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.where(post: @post).create
+      expect( @user.favorites.find_by_post_id(@post.id) ).to eq(favorite)
+    end
+  end
+end
